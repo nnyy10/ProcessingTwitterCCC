1de004ce9f69b391dd8bba5572170c2e1fe52b55
Naiyun Wu <naiyun1@hotmail.com>
2019-03-16 18:02:31 +1100

    first version

--- script/main.py
+++ script/main.py
@@ -1,8 +1,15 @@
+import os
 import sys
+import json
 
 
 def main():
     print("lol")
+    f = open(r'C:\Users\Naiyun\PycharmProjects\ProcessingTwitterCCC\data\reallyreallysmallTwitter.json')
+    data = json.load(f)
+
+    print(data[0]['geo']['coordinates'])
+    print(data[1]['geo']['coordinates'])
 
 if __name__ == '__main__':
     main()
