b2bc0f1ebc9e65a488454c23d06eb2e74a47c75f
Naiyun Wu <naiyun1@hotmail.com>
2019-03-16 18:19:44 +1100

    loaded both data files

--- script/main.py
+++ script/main.py
@@ -4,12 +4,17 @@ import json
 
 
 def main():
-    print("lol")
-    f = open(r'C:\Users\Naiyun\PycharmProjects\ProcessingTwitterCCC\data\reallyreallysmallTwitter.json')
+    f = open(r'C:\Users\Naiyun\PycharmProjects\ProcessingTwitterCCC\data\tinyTwitter.json', encoding="utf8")
     data = json.load(f)
+
+    f = open(r'C:\Users\Naiyun\PycharmProjects\ProcessingTwitterCCC\data\melbGrid.json', encoding="utf8")
+    melbourne_grid = json.load(f)
 
     print(data[0]['geo']['coordinates'])
     print(data[1]['geo']['coordinates'])
+
+    print(melbourne_grid["features"][])
+
 
 if __name__ == '__main__':
     main()
